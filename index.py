058f68942660c3c6b820b2a03c02568f2e1cb58d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-03-07 18:33:20 +0100

    edit to add web ui

--- index.py
+++ index.py
@@ -1,17 +1,22 @@
 import cv2
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/create/", methods=["POST"])
 def handle_create():
-    if "file" not in request.file:
-
-        return {
-            "Error": "File not found"
-        }
-    return "Created"
+    if request.method == "POST":
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file_ = request.files['file']
+        if file_.filename == '':
+            return redirect(request.url)
+        if file_:
+            image = cv2.imread(file_)
+            sketch_gray, sketch_color = cb2.pencilSketch(file_, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
+            return render_template("result.html", image=sketch_gray, image_c=sketch_color)
+    return render_template("index.html")
 
 
 image_file = "flower.jpeg"
